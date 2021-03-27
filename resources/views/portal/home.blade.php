aca812a6fe3df54495c69c196d85ab292f692eef
Lohan Luiz <lohanluiz12@gmail.com>
2021-03-26 23:14:24 -0400

    commit lohan: combustao

--- resources/views/portal/home.blade.php
+++ resources/views/portal/home.blade.php
@@ -20,7 +20,7 @@
 
             <!-- PHP sintaxe -->
             @if(date('H') >= 0 && date('H') <= 12)
-            <p> Bom dia!</p>
+            <p> Bom dia! COMBUSTAO</p>
             @elseif (date('H') >= 13 && date('H') <= 18)
             <p> Boa Tarde! rola amigos</p>
             @else
